10248f43eb64465084e8d142a15be6ceb3efab43
Amanda Freire <52572891+MandyFreire@users.noreply.github.com>
2025-07-14 20:52:18 -0300

    Update realtime_plot.py

--- dashboard/realtime_plot.py
+++ dashboard/realtime_plot.py
@@ -1,1 +1,26 @@
+import serial
+import pyqtgraph as pg
+from PyQt5 import QtWidgets
+
+arduino = serial.Serial('COM3', 9600)
+
+app = QtWidgets.QApplication([])
+win = QtWidgets.QMainWindow()
+plot = pg.PlotWidget()
+win.setCentralWidget(plot)
+win.show()
+
+x, y = [], []
+
+def update():
+    rpm = float(arduino.readline().decode().strip())
+    x.append(len(x))
+    y.append(rpm)
+    plot.plot(x, y, clear=True)
+
+timer = pg.QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(100)
+
+app.exec_()
 
